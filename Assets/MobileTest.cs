e3fd907936a1b6493a865e735b433ee89172ac83
oct0punk <elric.emile@gmail.com>
2023-09-30 12:47:25 +0200

    PostProcess

--- Assets/MobileTest.cs
+++ Assets/MobileTest.cs
@@ -5,18 +5,12 @@ using UnityEngine.UI;
 
 public class MobileTest : MonoBehaviour
 {
-    public Camera cam;
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
 
     // Update is called once per frame
     void Update()
     {
         if (Input.touches.Length == 0) return;
-        Vector3 pos = cam.ScreenToWorldPoint(Input.GetTouch(0).position);
+        Vector3 pos = Camera.main.ScreenToWorldPoint(Input.GetTouch(0).position);
         pos.z = 0;
         transform.position = pos;
     }
